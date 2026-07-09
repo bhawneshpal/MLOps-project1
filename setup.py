a52efda61a58c98894692fbad5cb0cf90e2923bd
bhawneshpal <bhawneshpal9038@gmail.com>
2026-07-09 22:13:46 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.2",
+    author="Bhawnesh pal",
+    author_email="bhawneshpal9038@gmail.com",
+    packages=find_packages()
+)
